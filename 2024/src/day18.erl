ad037e755a87432f383dc45918f6c26dc6bcf6f1
Julius Andrikonis <julius@andrikonis.lt>
2024-12-18 23:53:23 +0200

    Day 18 optimisation and tidying up

--- 2024/src/day18.erl
+++ 2024/src/day18.erl
@@ -4,7 +4,9 @@
 % Pagaliau išmokau vaiškčioti per žemėlapį. Užtruko tik užrašymas. Iš esmės,
 % kopijavau nuo 16 dienos ir išmečiau tai, ko nereikia. Antrą dalį dariau
 % primityviai. Gavosi greitai, bet nelabai efektyviai: apie 5 sekundes. Turiu
-% minčių, ką reikia patobulinti.
+% minčių, ką reikia patobulinti. Prisėdau. Patobulinau. Keli šimtai
+% milisekundžių dabar. Pakako prisiminti gerą kelią ir jį perskaičiuoti tik
+% jei ant jo esantį langelį sugadina.
 
 % (aoc_2024@JuliusErisataT14.erisata.lt)1> day18:solve_1("priv/day18-PVZ.txt", {7,7}, 12).
 % 22
@@ -17,7 +19,7 @@
 % (aoc_2024@JuliusErisataT14.erisata.lt)5> timer:tc(fun() -> day18:solve_1("priv/day18.txt", {71,71}, 1024) end).
 % {42086,438}
 % (aoc_2024@JuliusErisataT14.erisata.lt)6> timer:tc(fun() -> day18:solve_2("priv/day18.txt", {71,71}, 1024) end).
-% {4852024,"26,22"}
+% {161263,"26,22"}
 
 
 read_inputs(FileName) ->
@@ -27,14 +29,25 @@ read_inputs(FileName) ->
     end).
 
 
-place_corrupted(Indexes, Dimensions) ->
+index_to_map({Row, Col}) -> {Row+1, Col+1}.
+
+place_corrupted(Index, Map) ->
+    Map#{index_to_map(Index) => $#}.
+
+place_init_corrupted(Indexes, Dimensions) ->
     MapEmpty = utils:get_new_matrix($., Dimensions),
-    lists:foldl(fun({Row, Col}, Acc) ->
-        Acc#{{Row+1,Col+1} => $#}
+    lists:foldl(fun(Index, Acc) ->
+        place_corrupted(Index, Acc)
     end, MapEmpty, Indexes).
 
 
-walk_map([],                                  _,   _,          Distances) -> Distances;
+walk_map(Map, Dimensions) ->
+    walk_map({1,1}, Map,  Dimensions).
+
+walk_map(StartIndex, Map, Dimensions) ->
+    walk_map([StartIndex], Map, Dimensions, #{StartIndex => {0, []}}).
+
+walk_map([],              _,   _,          Distances) -> Distances;
 walk_map([Index | Tiles], Map, Dimensions, Distances) ->
     Directions = utils:direction_all(),
     {NewTiles, NewDistances} = lists:foldl(fun(NewDirection, {AccTiles, AccDistances}) ->
@@ -45,13 +58,14 @@ walk_map([Index | Tiles], Map, Dimensions, Distances) ->
                 case maps:get(NewIndex, Map) of
                     $# -> {AccTiles, AccDistances};
                     $. ->
-                        PrevDistance = maps:get(Index, Distances),
+                        {PrevDistance, PrevPath} = maps:get(Index, AccDistances),
                         NewDistance = PrevDistance + 1,
-                        OldDistance = maps:get(NewIndex, AccDistances, infinity),
+                        NewPath = [Index | PrevPath],
+                        {OldDistance, _} = maps:get(NewIndex, AccDistances, {infinity, undefined}),
                         case NewDistance < OldDistance of
                             true  ->
                                 NewAccTiles = AccTiles ++ [NewIndex],
-                                NewAccDistances = AccDistances#{NewIndex => NewDistance},
+                                NewAccDistances = AccDistances#{NewIndex => {NewDistance, NewPath}},
                                 {NewAccTiles, NewAccDistances};
                             false ->
                                 {AccTiles, AccDistances}
@@ -62,24 +76,35 @@ walk_map([Index | Tiles], Map, Dimensions, Distances) ->
     walk_map(NewTiles, Map, Dimensions, NewDistances).
 
 
+solve(FileName, Dimensions, Bytes) ->
+    Corrupted = lists:reverse(read_inputs(FileName)),
+    RealCorrupted = lists:sublist(Corrupted, Bytes),
+    Map = place_init_corrupted(RealCorrupted, Dimensions),
+    RemCorrupted = lists:nthtail(Bytes, Corrupted),
+    Distances = walk_map(Map, Dimensions),
+    {Distance, Path} = maps:get(Dimensions, Distances),
+    {RemCorrupted, Map, Distance, Path}.
+
+
+
 solve_1(FileName, Dimensions, Bytes) ->
-    Corrupted = read_inputs(FileName),
-    RealCorrupted = lists:sublist(lists:reverse(Corrupted), Bytes),
-    Map = place_corrupted(RealCorrupted, Dimensions),
-    Distances = walk_map([{1,1}], Map, Dimensions, #{{1,1} => 0}),
-    maps:get(Dimensions, Distances).
+    {_, _, Distance, _} = solve(FileName, Dimensions, Bytes),
+    Distance.
 
 
 solve_2(FileName, Dimensions, Bytes) ->
-    Corrupted = lists:reverse(read_inputs(FileName)),
-    RealCorrupted = lists:sublist(Corrupted, Bytes),
-    Map = place_corrupted(RealCorrupted, Dimensions),
-    Fun = fun Fun([{Row,Col}|Indexes], AccMap) ->
-        NewAccMap = AccMap#{{Row+1,Col+1} => $#},
-        Distances = walk_map([{1,1}], NewAccMap, Dimensions, #{{1,1} => 0}),
-        case maps:get(Dimensions, Distances, undefined) of
-            undefined -> lists:append(lists:join(",",[erlang:integer_to_list(Row), erlang:integer_to_list(Col)]));
-            _         -> Fun(Indexes, NewAccMap)
+    {RemCorrupted, Map, _, InitPath} = solve(FileName, Dimensions, Bytes),
+    Fun = fun Fun([{Row,Col}=Index | Indexes], AccMap, PathToEnd) ->
+        NewAccMap = place_corrupted(Index, AccMap),
+        case lists:member(index_to_map(Index), PathToEnd) of
+            true ->
+                NewDistances = walk_map(NewAccMap, Dimensions),
+                case maps:get(Dimensions, NewDistances, undefined) of
+                    undefined -> lists:append(lists:join(",",[erlang:integer_to_list(Row), erlang:integer_to_list(Col)]));
+                    {_, Path} -> Fun(Indexes, NewAccMap, Path)
+                end;
+            false ->
+                Fun(Indexes, NewAccMap, PathToEnd)
         end
     end,
-    Fun(lists:nthtail(Bytes, Corrupted), Map).
+    Fun(RemCorrupted, Map, InitPath).
