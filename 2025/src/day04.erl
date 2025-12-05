bc4716500bdcb821547f37b53b08086ec74b1242
Julius Andrikonis <julius@andrikonis.lt>
2025-12-06 01:24:36 +0200

    Day 4 and 5 code beauty improvements and comments

--- 2025/src/day04.erl
+++ 2025/src/day04.erl
@@ -1,12 +1,38 @@
 -module(day04).
 -export([solve_1/1, solve_2/1]).
 
+% Buvo lengviausias iš kol kas pasitaikiusių. Iš pradžių bandžiau galvoti
+% gudresnį algoritmą, bet paskaičiavau, kad 138*138 langelių nėra tiek jau
+% daug, ir nusprendžiau iš karto nekomplikuoti sau gyvenimo. Nebent vėliau.
+% Tas vėliau atėjo su antra dalimi. Pagalvojau, kad daug kartų eiti per tuos
+% pačius ~20 tūkstančių langelių gali būti ir daugoka. Nusprendžiau pasidaryti
+% ir pakeliui palaikyti tik rulonų indeksų sąrašą. Nežinau, ar pasiteisino
+% būtent tai, bet programa prasisuko. Tiesa, pirmą kartą šiais metais prieš
+% išvesdama atsakymą trumpai žagtelėjo.
 
-find_rolls(Matrix) ->
-    ja_erl_utils_matrix:foldf(fun
+% (aoc_2025@JuliusErisataT14.erisata.lt)1> day04:solve_1("priv/day04-PVZ.txt").
+% 13
+% (aoc_2025@JuliusErisataT14.erisata.lt)2> day04:solve_1("priv/day04.txt").
+% 1376
+% (aoc_2025@JuliusErisataT14.erisata.lt)3> day04:solve_2("priv/day04-PVZ.txt").
+% 43
+% (aoc_2025@JuliusErisataT14.erisata.lt)4> day04:solve_2("priv/day04.txt").
+% 8587
+% (aoc_2025@JuliusErisataT14.erisata.lt)5> timer:tc(fun() -> day04:solve_1("priv/day04.txt") end).
+% {78219,1376}
+% (aoc_2025@JuliusErisataT14.erisata.lt)6> timer:tc(fun() -> day04:solve_2("priv/day04.txt") end).
+% {346478,8587}
+
+
+read(FileName) ->
+    Lines = lists:reverse(ja_erl_utils_file:read_lines_no_new_line(FileName)),
+    Matrix = ja_erl_utils_matrix:get_char_matrix(Lines),
+    %ja_erl_utils_matrix:print_char_matrix(Matrix),
+    Rolls = ja_erl_utils_matrix:foldf(fun
         (_,     $., AccIn) -> AccIn;
         (Index, $@, AccIn) -> [Index|AccIn]
-    end, [], Matrix).
+    end, [], Matrix),
+    {Matrix, Rolls}.
 
 
 remove_rolls(RollIndexes, Matrix) ->
@@ -36,18 +62,13 @@ remove_rolls(RollIndexes, Matrix) ->
 
 
 solve_1(FileName) ->
-    Lines = lists:reverse(ja_erl_utils_file:read_lines_no_new_line(FileName)),
-    Matrix = ja_erl_utils_matrix:get_char_matrix(Lines),
-    Rolls = find_rolls(Matrix),
+    {Matrix, Rolls} = read(FileName),
     {RollsRemoved, _, _} = remove_rolls(Rolls, Matrix),
     erlang:length(RollsRemoved).
 
 
 solve_2(FileName) ->
-    Lines = lists:reverse(ja_erl_utils_file:read_lines_no_new_line(FileName)),
-    Matrix = ja_erl_utils_matrix:get_char_matrix(Lines),
-    %ja_erl_utils_matrix:print_char_matrix(Matrix),
-    Rolls = find_rolls(Matrix),
+    {Matrix, Rolls} = read(FileName),
     RemoveAllRollsFun = fun RemoveAllRollsFun(AccRollIndexes, AccMatrix, Count) ->
         {RollsRemoved, RollsRemaining, NewAccMatrix} = remove_rolls(AccRollIndexes, AccMatrix),
         case RollsRemoved of
--- 2025/src/day05.erl
+++ 2025/src/day05.erl
@@ -2,16 +2,27 @@
 -export([solve_1/1, solve_2/1]).
 
 
-% Aš jau pirmojoje dalyje galvojau jungti intervalos. Bet paskui nusprendžiau,
+% Aš jau pirmojoje dalyje galvojau jungti intervalus. Bet paskui nusprendžiau,
 % kad nereikia. Aš turiu išankstinių optimizacijų bėdą ir taip prisidarau sau
-% darbo. Tai pirmą dalį padariau paprastai. Bet antrojoje vis dėl to to
+% darbo. Tai pirmąją dalį padariau paprastai. Bet antrojoje vis dėl to to
 % prireikė. Tai nesunkiai padariau. Tik vieną klaidą įvėliau, kuri kainavo man
 % 5 minutes laiko. Žodžiu, lengva užduotis buvo.
 
 % (aoc_2025@JuliusErisataT14.erisata.lt)1> day05:solve_1("priv/day05-PVZ.txt").
+% 3
+% (aoc_2025@JuliusErisataT14.erisata.lt)2> day05:solve_1("priv/day05.txt").
+% 617
+% (aoc_2025@JuliusErisataT14.erisata.lt)3> day05:solve_2("priv/day05-PVZ.txt").
+% 14
+% (aoc_2025@JuliusErisataT14.erisata.lt)4> day05:solve_2("priv/day05.txt").
+% 338258295736104
+% (aoc_2025@JuliusErisataT14.erisata.lt)5> timer:tc(fun() -> day05:solve_1("priv/day05.txt") end).
+% {13167,617}
+% (aoc_2025@JuliusErisataT14.erisata.lt)6> timer:tc(fun() -> day05:solve_2("priv/day05.txt") end).
+% {11122,338258295736104}
 
 
-solve_1(FileName) ->
+read(FileName) ->
     [IdStrs, RangeStrs] = ja_erl_utils_file:read_line_groups_no_new_line(FileName, fun(Groups) -> Groups end, "\n"),
     Ranges = lists:sort(lists:map(fun(RangeStr) ->
         [From, To] = string:split(RangeStr, "-"),
@@ -19,18 +30,6 @@ solve_1(FileName) ->
     end, RangeStrs)),
     Ids = lists:map(fun erlang:list_to_integer/1, IdStrs),
     %ja_erl_utils_terminal:print("~p~n~p", [Ranges, Ids]),
-    ja_erl_utils_list:filter_count(fun(Id) ->
-        lists:any(fun({From, To}) -> From =< Id andalso Id =< To end, Ranges)
-    end, Ids).
-
-
-solve_2(FileName) ->
-    [_IdStrs, RangeStrs] = ja_erl_utils_file:read_line_groups_no_new_line(FileName, fun(Groups) -> Groups end, "\n"),
-    Ranges = lists:sort(lists:map(fun(RangeStr) ->
-        [From, To] = string:split(RangeStr, "-"),
-        {erlang:list_to_integer(From), erlang:list_to_integer(To)}
-    end, RangeStrs)),
-    %ja_erl_utils_terminal:print("~p", [Ranges]),
     JoinRangesFun = fun
         JoinRangesFun([], AccJoinedRanges) ->
             AccJoinedRanges;
@@ -47,4 +46,16 @@ solve_2(FileName) ->
     end,
     JoinedRanges = JoinRangesFun(Ranges, []),
     %ja_erl_utils_terminal:print("~p", [JoinedRanges]),
-    ja_erl_utils_list:map_sum(fun({From, To}) -> To - From + 1 end, JoinedRanges).
+    {JoinedRanges, Ids}.
+
+
+solve_1(FileName) ->
+    {Ranges, Ids} = read(FileName),
+    ja_erl_utils_list:filter_count(fun(Id) ->
+        lists:any(fun({From, To}) -> From =< Id andalso Id =< To end, Ranges)
+    end, Ids).
+
+
+solve_2(FileName) ->
+    {Ranges, _Ids} = read(FileName),
+    ja_erl_utils_list:map_sum(fun({From, To}) -> To - From + 1 end, Ranges).
